5c358ee1c033646197646c7fd68520ef90b3ac1b
PedroLudu <pedroaludugero243@gmail.com>
2026-05-06 23:23:08 -0300

    Update prog.js
    sla2

--- prog.js
+++ prog.js
@@ -1,43 +1,76 @@
-const SHEET_NAME = "estoque";
+const SHEET_NAME = "Página1";
 
-function doGet() {
-  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET_NAME);
-  const data = sheet.getDataRange().getValues();
+const sheet =
+  SpreadsheetApp.openById(
+    "1EqNHODFZ_XIDOahT1clXIhfs1ZzMw_OagdIm36YGwRM"
+  );
+
+function doGet(e) {
+
+  const aba = sheet.getSheetByName(SHEET_NAME);
+
+  if (e.parameter.rua) {
+
+    const rua = e.parameter.rua;
+    const coluna = e.parameter.coluna;
+    const altura = e.parameter.altura;
+    const item = e.parameter.item;
+
+    const rows = aba.getDataRange().getValues();
+
+    for (let i = 1; i < rows.length; i++) {
+
+      if (
+        rows[i][0] == rua &&
+        rows[i][1] == coluna &&
+        rows[i][2] == altura
+      ) {
+
+        aba.getRange(i + 1, 4).setValue(item);
+
+        return ContentService
+  .createTextOutput(
+    e.parameter.callback + '("Atualizado")'
+  )
+  .setMimeType(ContentService.MimeType.JAVASCRIPT);
+      }
+    }
+
+    aba.appendRow([
+      rua,
+      coluna,
+      altura,
+      item
+    ]);
+
+    return ContentService
+  .createTextOutput(
+    e.parameter.callback + '("Adicionado")'
+  )
+  .setMimeType(ContentService.MimeType.JAVASCRIPT);
+  }
+
+  const data = aba.getDataRange().getValues();
 
   const headers = data[0];
   const rows = data.slice(1);
 
   const result = rows.map(row => {
+
     let obj = {};
-    headers.forEach((h, i) => obj[h] = row[i]);
+
+    headers.forEach((h, i) => {
+      obj[h] = row[i];
+    });
+
     return obj;
   });
 
-  return ContentService
-    .createTextOutput(JSON.stringify(result))
-    .setMimeType(ContentService.MimeType.JSON);
-}
+  const callback = e.parameter.callback;
 
-function doPost(e) {
-  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET_NAME);
-  const data = JSON.parse(e.postData.contents);
-
-  const rows = sheet.getDataRange().getValues();
-
-  // verifica se posição já existe
-  for (let i = 1; i < rows.length; i++) {
-    if (
-      rows[i][0] == data.rua &&
-      rows[i][1] == data.coluna &&
-      rows[i][2] == data.altura
-    ) {
-      sheet.getRange(i + 1, 4).setValue(data.item);
-      return ContentService.createTextOutput("Atualizado");
-    }
-  }
-
-  // se não existir, adiciona
-  sheet.appendRow([data.rua, data.coluna, data.altura, data.item]);
-
-  return ContentService.createTextOutput("Adicionado");
+return ContentService
+  .createTextOutput(
+    callback + "(" + JSON.stringify(result) + ")"
+  )
+  .setMimeType(ContentService.MimeType.JAVASCRIPT);
 }
